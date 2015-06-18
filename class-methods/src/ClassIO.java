8fb45d8d285a6a6ee8ae975595f04ad7e236def8
Jim Shaw <jimxshaw@gmail.com>
2015-06-17 20:40:09 -0500

    main() pt 1

--- class-methods/src/ClassIO.java
+++ class-methods/src/ClassIO.java
@@ -18,8 +18,16 @@ public class ClassIO {
 		JOptionPane.showMessageDialog(null, s1, s2, JOptionPane.PLAIN_MESSAGE);
 	}
 	public static void main(String[] args) {
+		Scanner input = getInput("Enter 2 integers: ");
+		int a = input.nextInt();
+		int b = input.nextInt();
 		
-
+		if (a > 0 && b > 0) showMessage(String.format("Sum:%7d", (a + b)));
+		else showMessage("Invalid numbers");
+		
+		
+		
+		System.exit(0);
 	}
 
 }
