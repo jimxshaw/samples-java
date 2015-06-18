c8336e00c511ef8ba7811606f49668ae01870aa3
Jim Shaw <jimxshaw@gmail.com>
2015-06-17 20:27:01 -0500

    added methods

--- class-methods/src/ClassIO.java
+++ class-methods/src/ClassIO.java
@@ -6,6 +6,17 @@ public class ClassIO {
 	public static Scanner getInput(String prompt) {
 		return new Scanner(JOptionPane.showInputDialog(prompt));
 	}
+	public static Scanner getConsoleInput(String prompt) {
+		return new Scanner(System.in);
+	}
+	public static void showMessage(String s) {
+		System.out.println(s);
+		JOptionPane.showMessageDialog(null, s);
+	}
+	public static void showMessage(String s1, String s2) {
+		System.out.println(s1);
+		JOptionPane.showMessageDialog(null, s1, s2, JOptionPane.PLAIN_MESSAGE);
+	}
 	public static void main(String[] args) {
 		
 
