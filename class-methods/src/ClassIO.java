013d987a81c801a603bac2c2ca2900a66808616c
Jim Shaw <jimxshaw@gmail.com>
2015-06-17 20:58:15 -0500

    finished main

--- class-methods/src/ClassIO.java
+++ class-methods/src/ClassIO.java
@@ -7,6 +7,7 @@ public class ClassIO {
 		return new Scanner(JOptionPane.showInputDialog(prompt));
 	}
 	public static Scanner getConsoleInput(String prompt) {
+		System.out.println(prompt);
 		return new Scanner(System.in);
 	}
 	public static void showMessage(String s) {
@@ -25,7 +26,13 @@ public class ClassIO {
 		if (a > 0 && b > 0) showMessage(String.format("Sum:%7d", (a + b)));
 		else showMessage("Invalid numbers");
 		
+		Scanner scan = getConsoleInput("Enter 2 real numbers: ");
+		float x = scan.nextFloat();
+		float y = scan.nextFloat();
 		
+		if (x < 0 && y > -1 || x > -1 && y < 0) showMessage(String.format("Product:%10.2f", (x * y)), "Your Result");
+		else if (x < 0 && y < 0) showMessage(String.format("Quotient:%10.2f", (x / y)), "Your Result");
+		else showMessage("Invalid numbers");
 		
 		System.exit(0);
 	}
