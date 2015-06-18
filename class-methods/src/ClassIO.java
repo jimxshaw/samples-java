eaf4fe0a6fd314402e5706c3b4bc81320708765f
Jim Shaw <jimxshaw@gmail.com>
2015-06-17 20:20:05 -0500

    initial commit

---
+++ class-methods/src/ClassIO.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.util.Scanner;
+import java.awt.Font;
+
+public class ClassIO {
+	public static Scanner getInput(String prompt) {
+		return new Scanner(JOptionPane.showInputDialog(prompt));
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
