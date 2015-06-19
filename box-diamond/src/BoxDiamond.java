fcc2a602a306f310b423020371d931697db6d02b
Jim Shaw <jimxshaw@gmail.com>
2015-06-18 21:27:53 -0500

    added input-output methods

--- box-diamond/src/BoxDiamond.java
+++ box-diamond/src/BoxDiamond.java
@@ -1,9 +1,24 @@
 import javax.swing.*;
+
 import java.util.Scanner;
 import java.awt.Font;
 
 public class BoxDiamond {
-
+	public static Scanner getInput(String prompt) {
+		return new Scanner(JOptionPane.showInputDialog(prompt));
+	}
+	public static Scanner getConsoleInput(String prompt) {
+		System.out.println(prompt);
+		return new Scanner(System.in);
+	}
+	public static void showMessage(String s) {
+		System.out.println(s);
+		JOptionPane.showMessageDialog(null, s);
+	}
+	public static void showMessage(String s1, String s2) {
+		System.out.println(s1);
+		JOptionPane.showMessageDialog(null, s1, s2, JOptionPane.PLAIN_MESSAGE);
+	}
 	public static void main(String[] args) {
 		
 
