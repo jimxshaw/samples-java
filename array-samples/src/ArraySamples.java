118cf9a56bc0f1b3177a834e1c01f319f1c2f17a
Jim Shaw <jimxshaw@gmail.com>
2015-06-25 14:31:32 -0500

    added input-output methods

--- array-samples/src/ArraySamples.java
+++ array-samples/src/ArraySamples.java
@@ -4,6 +4,25 @@ import java.util.StringTokenizer;
 import java.awt.Font;
 
 public class ArraySamples {
+	public static Scanner getInput(String prompt) {
+		return new Scanner(JOptionPane.showInputDialog(prompt));
+	}
+	public static Scanner getConsoleInput(String prompt) {
+		System.out.println(prompt);
+		return new Scanner(System.in);
+	}
+	public static void showMessage(String s) {
+		JTextArea jta = new JTextArea(s);
+		jta.setFont(new Font("Lucida Console", Font.PLAIN, 16));
+		System.out.println(s);
+		JOptionPane.showMessageDialog(null, jta);
+	}
+	public static void showMessage(String message, String title) {
+		JTextArea jta = new JTextArea(message);
+		jta.setFont(new Font("Lucida Console", Font.PLAIN, 35));
+		System.out.println(message);
+		JOptionPane.showMessageDialog(null, jta, title, JOptionPane.PLAIN_MESSAGE);
+	}
 	public static int[] getData(String input) {
 		StringTokenizer st = new StringTokenizer(input);
 		int x[] = new int[st.countTokens()];
@@ -37,18 +56,23 @@ public class ArraySamples {
 		return x;
 	}
 	public static int[] sortByBubble(int x[]) {
-		for (int i = 0; i < x.length - 1; i++) {
-			for (int j = 0; j < x.length - 1; j++) {
+		for (int i = 0; i < x.length - 1; i++)
+			for (int j = 0; j < x.length - 1; j++)
 				if (x[j + 1] > x[j]) {
 					int temp = x[j];
 					x[j] = x[j + 1];
 					x[j + 1] = temp;
 				}
-			}
-		}
 		return x;
 	}
 	public static void main(String[] args) {
+		String input = JOptionPane.showInputDialog("Enter 1 or more grades");
+		
+		int x[] = getData(input);
+		
+		showMessage("Array: " + putArray(x) + 
+				"\nSorted: " + putArray(sortByBubble(x)) + 
+				String.format("\nAverage:%4.2f ", getAverage(x)));
 		
 		System.exit(0);
 	}
