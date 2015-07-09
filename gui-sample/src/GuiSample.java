6fcbc52c0d977b9ec0b20af2edb6f4f9db0cda46
Jim Shaw <jimxshaw@gmail.com>
2015-07-08 21:12:20 -0500

    added methods

--- gui-sample/src/GuiSample.java
+++ gui-sample/src/GuiSample.java
@@ -1,115 +1,126 @@
-
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
+import util.IO;
 
 public class GuiSample extends JFrame {
-    StudentList list = new StudentList();
-    
-    class StudentPanel extends JPanel {
 
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            
-            Graphics2D g2d = (Graphics2D)g;
-            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            
-            GradientPaint gp = new java.awt.GradientPaint(0, 0, Color.white, getWidth(), getHeight(), new Color(75, 255, 75));
-            g2d.setPaint(gp);
-            g2d.fillRect(0, 0, getWidth(), getHeight());
-            
-            g.setFont(new Font("Lucida Console", Font.PLAIN, 50));
-            g.setColor(Color.blue);
-            g.drawString("Names", 20, 50);
-            g.drawString("Grades", 240, 50);
-            FontMetrics fm = g.getFontMetrics();
-            for (int i = 0; i < list.length(); i++) {
-                Student tmp = list.get(i);
-                String s = tmp.grade + "";
-                int w = fm.stringWidth(s);
-                g.drawString(tmp.name, 20, 100 + i * 50);
-                g.drawString(s, 400 - w, 100 + i * 50);
-            }
-        }
-    }
+	StudentList list = new StudentList();
 
-    public GuiSample() {
-        // Gui title
-        super("Super Gui App");
+	class StudentPanel extends JPanel {
 
-        // Menus need 3 things
-        // 1. a MenuBar 2. Menu(s) 3. MenuItem(s)
-        JMenuBar jmb = new JMenuBar();
-        setJMenuBar(jmb);
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
 
-        // File Menu
-        JMenu jm = jmb.add(new JMenu("File"));
-        jm.setMnemonic('F');
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+					RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Shortcuts are different between a JMenu and a JMenuItem
-        JMenuItem jmi = jm.add(new JMenuItem("New...", 'N'));
-        jmi.addActionListener(e -> {
-            String input = JOptionPane.showInputDialog("Enter 1 or more students");
-            if (input != null) {
-                list.getData(input);
-                repaint();
-            }
-        });
-        
-        jmi = jm.add(new JMenuItem("Add...", 'A'));
-        jmi.addActionListener(e -> {
-        	String input = JOptionPane.showInputDialog("Enter a student");
-        	if (input != null) {
-        		list.appendData(input);
-        		repaint();
-        	}
-        });
-        
-        jm.addSeparator();
-        jmi = jm.add(new JMenuItem("Exit", 'X'));
-        // lambda expression
-        jmi.addActionListener(e -> System.exit(0));
+			GradientPaint gp = new java.awt.GradientPaint(0, 0, Color.white,
+					getWidth(), getHeight(), new Color(75, 255, 75));
+			g2d.setPaint(gp);
+			g2d.fillRect(0, 0, getWidth(), getHeight());
 
-        // Sort Menu
-        jm = jmb.add(new JMenu("Sort"));
-        jm.setMnemonic('S');
-        jmi = jm.add(new JMenuItem("Name", 'N'));
-        jmi.addActionListener(e -> {
-            list.sortNames();
-            repaint();
-        });
-        
-        jmi = jm.add(new JMenuItem("Grade", 'G'));
-        jmi.addActionListener(e -> {
-        	list.sortGrades();
-        	repaint();
-        });
+			g.setFont(new Font("Lucida Console", Font.PLAIN, 50));
+			g.setColor(Color.blue);
+			g.drawString("NAMES", 20, 50);
+			g.drawString("GRADES", 240, 50);
+			g.drawString("BREAKDOWN", 500, 50);
+			FontMetrics fm = g.getFontMetrics();
+			for (int i = 0; i < list.length(); i++) {
+				Student tmp = list.get(i);
+				String s = tmp.grade + "";
+				int w = fm.stringWidth(s);
+				g.drawString(tmp.name, 20, 100 + i * 50);
+				g.drawString(s, 400 - w, 100 + i * 50);
 
-        // Help Menu
-        jm = jmb.add(new JMenu("Help"));
-        jm.setMnemonic('H');
-        jmi = jm.add(new JMenuItem("About", 'A'));
+				int highest = list.getHighest();
+				int lowest = list.getLowest();
+				float average = list.getAverage();
 
-        jmi.addActionListener(e -> JOptionPane.showMessageDialog(GuiSample.this,
-                new JLabel("<html><big><center>"
-                        + "Super Gui App v0.1.1<br>"
-                        + "Copyright &copy; 2015 James Shaw<br>"
-                        + "All Rights Reserved"
-                        + "</center></big></html>"),
-                "About",
-                JOptionPane.PLAIN_MESSAGE));
-        
-        this.add(new StudentPanel());
+				g.drawString("Highest: " + highest, 500, 100);
+				g.drawString("Lowest: " + lowest, 500, 200);
+				g.drawString("Average: " + average, 500, 300);
+			}
+		}
+	}
 
-        // Gui configuration
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 600, 500);
-        setVisible(true);
-    }
+	public GuiSample() {
+		// Gui title
+		super("Super Gui App");
 
-    public static void main(String[] args) {
-        new GuiSample();
+		// Menus need 3 things
+		// 1. a MenuBar 2. Menu(s) 3. MenuItem(s)
+		JMenuBar jmb = new JMenuBar();
+		setJMenuBar(jmb);
 
-    }
+		// File Menu
+		JMenu jm = jmb.add(new JMenu("File"));
+		jm.setMnemonic('F');
+
+		// Shortcuts are different between a JMenu and a JMenuItem
+		JMenuItem jmi = jm.add(new JMenuItem("New...", 'N'));
+		jmi.addActionListener(e -> {
+			String input = JOptionPane
+					.showInputDialog("Enter 1 or more students");
+			if (input != null) {
+				list.getData(input);
+				repaint();
+			}
+		});
+
+		jmi = jm.add(new JMenuItem("Add...", 'A'));
+		jmi.addActionListener(e -> {
+			String input = JOptionPane.showInputDialog("Enter a student");
+			if (input != null) {
+				list.appendData(input);
+				repaint();
+			}
+		});
+
+		jm.addSeparator();
+		jmi = jm.add(new JMenuItem("Exit", 'X'));
+		// lambda expression
+		jmi.addActionListener(e -> System.exit(0));
+
+		// Sort Menu
+		jm = jmb.add(new JMenu("Sort"));
+		jm.setMnemonic('S');
+		jmi = jm.add(new JMenuItem("Name", 'N'));
+		jmi.addActionListener(e -> {
+			list.sortNames();
+			repaint();
+		});
+
+		jmi = jm.add(new JMenuItem("Grade", 'G'));
+		jmi.addActionListener(e -> {
+			list.sortGrades();
+			repaint();
+		});
+
+		// Help Menu
+		jm = jmb.add(new JMenu("Help"));
+		jm.setMnemonic('H');
+		jmi = jm.add(new JMenuItem("About", 'A'));
+
+		jmi.addActionListener(e -> JOptionPane.showMessageDialog(
+				GuiSample.this, new JLabel("<html><big><center>"
+						+ "Super Gui App v0.1.1<br>"
+						+ "Copyright &copy; 2015 James Shaw<br>"
+						+ "All Rights Reserved" + "</center></big></html>"),
+				"About", JOptionPane.PLAIN_MESSAGE));
+
+		this.add(new StudentPanel());
+
+		// Gui configuration
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(1200, 200, 1000, 1000);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new GuiSample();
+
+	}
 
 }
--- gui-sample/src/StudentList.java
+++ gui-sample/src/StudentList.java
@@ -1,61 +1,103 @@
 import javax.swing.*;
 import java.util.*;
+import util.IO;
 
 class Student {
-    String name;
-    int grade;
-    
-    public Student(String name, int grade) {
-        this.name = name;
-        this.grade = grade;
-    }
+
+	String name;
+	int grade;
+
+	public Student(String name, int grade) {
+		this.name = name;
+		this.grade = grade;
+	}
 }
 
 public class StudentList {
-    Student [] students = new Student[0];
-    
-    public int length() {
-        return students.length;
-    }
-    public Student get(int n) {
-        return students[n];
-    }
-    
-    public void getData(String input) {
-        StringTokenizer st = new StringTokenizer(input);
-        students = new Student[st.countTokens() / 2];
-        for (int i = 0; st.hasMoreTokens(); i++) {
-            String name = st.nextToken();
-            int grade = Integer.parseInt(st.nextToken());
-            students[i] = new Student(name, grade);
-        }
-    }
-    
-    public void appendData(String input) {
-    	StringTokenizer st = new StringTokenizer(input);
-    	String name = st.nextToken();
-        int grade = Integer.parseInt(st.nextToken());
-        students[students.length - 1] = new Student(name, grade);
-    }
-    
-    public void sortNames() {
-        for (int i = 0; i < length() - 1; i++)
-            for (int j = 0; j < length() - 1; j++)
-                if (get(j + 1).name.compareTo(get(j).name) < 0) {
-                    Student tmp = students[j + 1];
-                    students[j + 1] = students[j];
-                    students[j] = tmp;
-                } 
-    }
-    
-    public void sortGrades() {
-    	for (int i = 0; i < length() - 1; i++)
-    		for (int j = 0; j < length() - 1; j++)
-    			if (get(j + 1).grade > get(j).grade) {
-    				Student tmp = students[j + 1];
-                    students[j + 1] = students[j];
-                    students[j] = tmp;
-    			}
-    }
-}
 
+	Student[] students = new Student[0];
+
+	public int length() {
+		return students.length;
+	}
+
+	public Student get(int n) {
+		return students[n];
+	}
+
+	public void getData(String input) {
+		StringTokenizer st = new StringTokenizer(input);
+		students = new Student[st.countTokens() / 2];
+		for (int i = 0; st.hasMoreTokens(); i++) {
+			String name = st.nextToken();
+			int grade = Integer.parseInt(st.nextToken());
+			students[i] = new Student(name, grade);
+		}
+	}
+
+	public void appendData(String input) {
+		StringTokenizer st = new StringTokenizer(input);
+		Student[] students2 = students;
+		students = new Student[students2.length + (st.countTokens() / 2)];
+		for (int i = 0; i < students2.length; i++) {
+			students[i] = students2[i];
+		}
+		for (int i = 0; st.hasMoreTokens(); i++) {
+			String name = st.nextToken();
+			int grade = Integer.parseInt(st.nextToken());
+			students[students2.length + i] = new Student(name, grade);
+		}
+	}
+
+	public void sortNames() {
+		for (int i = 0; i < length() - 1; i++) {
+			for (int j = 0; j < length() - 1; j++) {
+				if (get(j + 1).name.compareTo(get(j).name) < 0) {
+					Student tmp = students[j + 1];
+					students[j + 1] = students[j];
+					students[j] = tmp;
+				}
+			}
+		}
+	}
+
+	public void sortGrades() {
+		for (int i = 0; i < length() - 1; i++) {
+			for (int j = 0; j < length() - 1; j++) {
+				if (get(j + 1).grade > get(j).grade) {
+					Student tmp = students[j + 1];
+					students[j + 1] = students[j];
+					students[j] = tmp;
+				}
+			}
+		}
+	}
+
+	public int getHighest() {
+		int highest = get(0).grade;
+		for (int i = 0; i < length(); i++) {
+			if (get(i).grade > highest) {
+				highest = get(i).grade;
+			}
+		}
+		return highest;
+	}
+
+	public int getLowest() {
+		int lowest = get(0).grade;
+		for (int i = 0; i < length(); i++) {
+			if (get(i).grade < lowest) {
+				lowest = get(i).grade;
+			}
+		}
+		return lowest;
+	}
+
+	public float getAverage() {
+		float total = 0;
+		for (int i = 0; i < length(); i++) {
+			total += get(i).grade;
+		}
+		return total / length();
+	}
+}
