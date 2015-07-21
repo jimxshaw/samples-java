63136196c6a44b5b5a38cd455b7e580ae3959b76
Jim Shaw <jimxshaw@gmail.com>
2015-07-20 20:49:59 -0500

    formatted currency

---
+++ program1/src/Program1.java
@@ -0,0 +1,137 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.text.*;
+import java.awt.event.*;
+import employee.*;
+
+public class Program1 extends JFrame {
+
+    EmployeeList list = new EmployeeList();
+
+    class GuiPanel extends JPanel {
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(
+                    RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            GradientPaint gp = new GradientPaint(0, 0, Color.white, 0, getHeight(), new Color(175, 255, 175));
+            g2d.setPaint(gp);
+            g2d.fillRect(0, 0, getWidth(), getHeight());
+            g.setColor(Color.blue);
+            g.setFont(new Font("Calibri", Font.BOLD, 50));
+            g.drawString("NAME", 20, 80);
+            g.drawString("AGE", 250, 80);
+            g.drawString("INCOME", 420, 80);
+            g.drawString("BREAKDOWN", 670, 80);
+            FontMetrics fm = g.getFontMetrics();
+            for (int i = 0; i < list.length(); i++) {
+                Employee temp = list.get(i);
+                g.drawString(temp.getName(), 20, 160 + i * 80);
+                String s = String.format("%d", temp.getAge());
+                int w = fm.stringWidth(s);
+                g.drawString(s, 330 - w, 160 + i * 80);
+                NumberFormat nf = NumberFormat.getCurrencyInstance();
+                nf.setMaximumFractionDigits(0);
+                s = nf.format(temp.getIncome());
+                w = fm.stringWidth(s);
+                g.drawString(s, 590 - w, 160 + i * 80);
+                
+                float highest = list.getHighestIncome();
+                float lowest = list.getLowestIncome();
+                float average = list.getAverageIncome();
+                
+                g.drawString("Highest: $" + highest, 670, 160);
+                g.drawString("Lowest: $" + lowest, 670, 260);
+                g.drawString("Average: $" + average, 670, 360);
+            }
+        }
+    }
+
+    // constructor
+    public Program1() {
+        super("Super Gui App");
+
+        JMenuBar jmb = new JMenuBar();
+        setJMenuBar(jmb);
+
+        // File menu
+        JMenu jm = jmb.add(new JMenu("File"));
+        jm.setMnemonic('F');
+        JMenuItem jmi = jm.add(new JMenuItem("New...", 'N'));
+        jmi.addActionListener(e -> {
+            String input = JOptionPane.showInputDialog(Program1.this, "Enter 1 or more salary employees");
+            if (input != null) {
+                list.getData(input);
+                repaint();
+            }
+        });
+
+        jmi = jm.add(new JMenuItem("Add...", 'A'));
+        jm.addSeparator();
+        jmi.addActionListener(e -> {
+            String input = JOptionPane.showInputDialog("Enter additional salary employees");
+            if (input != null) {
+                list.appendData(input);
+                repaint();
+            }
+        });
+        
+        jmi = jm.add(new JMenuItem("Exit", 'X'));
+        jmi.addActionListener(e -> System.exit(0));
+
+        // Sort menu
+        jm = jmb.add(new JMenu("Sort"));
+        jm.setMnemonic('S');
+        ButtonGroup bg = new ButtonGroup();
+        jmi = jm.add(new JRadioButtonMenuItem("Name"));
+        bg.add(jmi);
+        jmi.addActionListener(e -> {
+            list.sortNames();
+            repaint();
+        });
+        jmi.setMnemonic('N');
+        jmi = jm.add(new JRadioButtonMenuItem("Age"));
+        bg.add(jmi);
+        jmi.addActionListener( e -> {
+            list.sortAge();
+            repaint();
+        });
+        jmi.setMnemonic('A');
+        jmi = jm.add(new JRadioButtonMenuItem("Income"));
+        bg.add(jmi);
+        jmi.addActionListener( e -> {
+            list.sortIncome();
+            repaint();
+        });
+        jmi.setMnemonic('I');
+
+        // Help menu
+        jm = jmb.add(new JMenu("Help"));
+        jm.setMnemonic('H');
+        jmi = jm.add(new JMenuItem("About", 'A'));
+        jmi.addActionListener(e -> JOptionPane.showMessageDialog(Program1.this,
+                new JLabel("<html><center><big>"
+                        + "Gui App v0.1<br>"
+                        + "Copyright \u00a9 2015 James Shaw<br>"
+                        + "All Rights Reserved"
+                        + "</big></center></html>"),
+                "About",
+                JOptionPane.PLAIN_MESSAGE
+        ));
+
+        this.add(new GuiPanel());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(1000, 250, 1200, 1000);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Program1();
+    }
+
+}
+
+
---
+++ program1/src/employee/EmployeeList.java
@@ -0,0 +1,103 @@
+package employee;
+
+import java.util.*;
+
+public class EmployeeList {
+
+    ArrayList<Employee> employees = new ArrayList<Employee>();
+
+    public int length() {
+        return employees.size();
+    }
+
+    public Employee get(int index) {
+        return employees.get(index);
+    }
+    
+    public void getData(String input) {
+        employees.clear();
+        Scanner scan = new Scanner(input);
+        for (; scan.hasNext();) {
+            String name = scan.next();
+            int age = scan.nextInt();
+            float income = scan.nextFloat();
+            employees.add(new SalaryEmployee(name, age, income));
+        }
+    }
+
+    public void appendData(String input) {
+        Scanner scan = new Scanner(input);
+        for (; scan.hasNext();) {
+            String name = scan.next();
+            int age = scan.nextInt();
+            float income = scan.nextFloat();
+            employees.add(new SalaryEmployee(name, age, income));
+        }
+    }
+
+    public void sortNames() {
+        for (int i = 0; i < length() - 1; i++) {
+            for (int j = 0; j < length() - 1; j++) {
+                if (get(j + 1).getName().compareTo(get(j).getName()) < 0) {
+                    Employee temp = employees.get(j);
+                    employees.set(j, employees.get(j + 1));
+                    employees.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    public void sortAge() {
+        for (int i = 0; i < length() - 1; i++) {
+            for (int j = 0; j < length() - 1; j++) {
+                if (get(j + 1).getAge() < get(j).getAge()) {
+                    Employee temp = employees.get(j);
+                    employees.set(j, employees.get(j + 1));
+                    employees.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    public void sortIncome() {
+        for (int i = 0; i < length() - 1; i++) {
+            for (int j = 0; j < length() - 1; j++) {
+                if (get(j + 1).getIncome() > get(j).getIncome()) {
+                    Employee temp = employees.get(j);
+                    employees.set(j, employees.get(j + 1));
+                    employees.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    public float getHighestIncome() {
+        float highest = get(0).getIncome();
+        for (int i = 0; i < length(); i++) {
+            if (get(i).getIncome() > highest) {
+                highest = get(i).getIncome();
+            }
+        }
+        return highest;
+    }
+
+    public float getLowestIncome() {
+        float lowest = get(0).getIncome();
+        for (int i = 0; i < length(); i++) {
+            if (get(i).getIncome() < lowest) {
+                lowest = get(i).getIncome();
+            }
+        }
+        return lowest;
+    }
+
+    public float getAverageIncome() {
+        float total = 0;
+        for (int i = 0; i < length(); i++) {
+            total += get(i).getIncome();
+        }
+        return total / length();
+    }
+}
+
+
