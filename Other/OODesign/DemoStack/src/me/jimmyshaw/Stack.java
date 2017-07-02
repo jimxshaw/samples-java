210450f0a7fd7634e2572c8587a141693a8143a8
Jim Shaw <jimxshaw@gmail.com>
2017-07-02 16:35:50 -0500

    Refactored Stack to that it no longer extends an ArrayList but contains an ArrayList.

--- Other/OODesign/DemoStack/src/me/jimmyshaw/Stack.java
+++ Other/OODesign/DemoStack/src/me/jimmyshaw/Stack.java
@@ -2,18 +2,22 @@ package me.jimmyshaw;
 
 import java.util.*;
 
-class Stack<T> extends ArrayList<T> {
+class Stack<T> {
+    ArrayList<T> contents = new ArrayList<T>();
+
     private int stackPointer = 0;
 
     public void push(T item) {
-        add(stackPointer++, item);
+        contents.add(stackPointer++, item);
     }
 
     public T pop() {
-        return remove(--stackPointer);
+        return contents.remove(--stackPointer);
     }
 
     public static void main(String[] args) {
         Stack<String> myStack = new Stack<String>();
+        myStack.push("A");
+        String s = myStack.pop();
     }
 }
