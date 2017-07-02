8664fd0fc7460ae3f199bbfff9f0eeea5a8e36b1
Jim Shaw <jimxshaw@gmail.com>
2017-07-02 16:42:14 -0500

    Added subclass that extends stack.

--- Other/OODesign/DemoStack/src/me/jimmyshaw/Stack.java
+++ Other/OODesign/DemoStack/src/me/jimmyshaw/Stack.java
@@ -15,9 +15,44 @@ class Stack<T> {
         return contents.remove(--stackPointer);
     }
 
+    public void pushMany(T[] items) {
+        for (T item : items) {
+            push(item);
+        }
+    }
+
+    public int size() {
+        return contents.size();
+    }
+
     public static void main(String[] args) {
         Stack<String> myStack = new Stack<String>();
         myStack.push("A");
         String s = myStack.pop();
     }
 }
+
+class MonitorableStack<T> extends Stack<T> {
+    private int highWaterMark = 0;
+
+    public int maximumSizeSoFar() {
+        return highWaterMark;
+    }
+
+    @Override
+    public void push(T item) {
+        if (size() > highWaterMark) {
+            highWaterMark = size();
+        }
+        super.push(item);
+    }
+
+    // inherit pop() and pushMany()...
+}
+
+
+
+
+
+
+
