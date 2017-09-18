ab237413a281519665f5dceb1e661a43df7fbe3f
Jim Shaw <jimxshaw@gmail.com>
2017-09-17 21:32:07 -0500

    Implement basic spring boot demo.

--- Other/SpringBootDemo/src/main/java/com/sample/demo/DemoApplication.java
+++ Other/SpringBootDemo/src/main/java/com/sample/demo/DemoApplication.java
@@ -2,11 +2,21 @@ package com.sample.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.sample"})
+@RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RequestMapping(value = "/greeting")
+    public String sayHello() {
+
+        return "Hello Spring Boot!";
+    }
 }
