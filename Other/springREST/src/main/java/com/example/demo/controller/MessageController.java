19ec27bba75ab61056763afc1dda4d8f2e1cdd37
Jim Shaw <jimxshaw@gmail.com>
2018-10-21 19:19:07 -0500

    Add echo http post method.

--- Other/springREST/src/main/java/com/example/demo/controller/MessageController.java
+++ Other/springREST/src/main/java/com/example/demo/controller/MessageController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Message;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +14,8 @@ public class MessageController {
         return new Message("This is the first message!");
     }
 
-    
+    @PostMapping("/message")
+    public Message echo(@RequestBody Message message) {
+        return message;
+    }
 }
