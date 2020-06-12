cb8308eaf33966fdf02605a3ae2f45cc032299c9
Jim Shaw <jimxshaw@gmail.com>
2020-06-11 22:13:14 -0500

    Output filter into text files.

--- Other/flink-sample-app/src/main/java/com/github/flink/FilterMovies.java
+++ Other/flink-sample-app/src/main/java/com/github/flink/FilterMovies.java
@@ -37,7 +37,9 @@ public class FilterMovies {
             }
         });
 
-        filteredMovies.print();
+        filteredMovies.writeAsText("filter-output");
+
+        env.execute();
     }
 }
 
