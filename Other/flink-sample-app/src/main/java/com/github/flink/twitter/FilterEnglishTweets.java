10f0b524a6e164c1aedd4c14c06b5be43a7a4b49
Jim Shaw <jimxshaw@gmail.com>
2020-06-17 22:41:11 -0500

    Update gitignore, add Tweet class and FilterEnglishTweets class.

--- Other/flink-sample-app/src/main/java/com/github/flink/twitter/FilterEnglishTweets.java
+++ Other/flink-sample-app/src/main/java/com/github/flink/twitter/FilterEnglishTweets.java
@@ -1,11 +1,29 @@
 package com.github.flink.twitter;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.hadoop.hdfs.server.namenode.ha.proto.HAZKInfoProtos;
+import org.apache.flink.streaming.connectors.twitter.TwitterSource;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.Map;
+import java.util.Properties;
 
 public class FilterEnglishTweets {
     public static void main(String[] args) throws Exception {
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        Yaml yaml = new Yaml();
+        Reader yamlFile = new FileReader("secrets.yml");
+
+        Map<String, Object> yamlMaps = yaml.load(yamlFile);
+
+        Properties props = new Properties();
+        props.setProperty(TwitterSource.CONSUMER_KEY, yamlMaps.get("API_KEY").toString());
+        props.setProperty(TwitterSource.CONSUMER_SECRET, yamlMaps.get("API_KEY_SECRET").toString());
+        props.setProperty(TwitterSource.TOKEN, yamlMaps.get("BEARER_TOKEN").toString());
+
+        env.addSource(new TwitterSource(props));
     }
 }
 
