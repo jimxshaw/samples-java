f9bd4bcfc0c7ffc06688ab2b4cbdc4a13cf9625f
Jim Shaw <jimxshaw@gmail.com>
2020-06-18 23:03:38 -0500

    Add MapToTweet inner class, add map and filter to env.

--- Other/flink-sample-app/src/main/java/com/github/flink/twitter/FilterEnglishTweets.java
+++ Other/flink-sample-app/src/main/java/com/github/flink/twitter/FilterEnglishTweets.java
@@ -1,7 +1,11 @@
 package com.github.flink.twitter;
 
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.twitter.TwitterSource;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileReader;
@@ -25,9 +29,32 @@ public class FilterEnglishTweets {
         props.setProperty(TwitterSource.TOKEN_SECRET, yamlMaps.get("ACCESS_TOKEN_SECRET").toString());
 
         env.addSource(new TwitterSource(props))
-           .print();
+                .map(new MapToTweet())
+                .filter(new FilterFunction<Tweet>() {
+                    @Override
+                    public boolean filter(Tweet tweet) throws Exception {
+                        return tweet.getLanguage().equals("en");
+                    }
+                })
+                .print();
 
         env.execute();
+    }
+
+    private static class MapToTweet implements MapFunction<String, Tweet> {
+        static private final ObjectMapper mapper = new ObjectMapper();
+
+        @Override
+        public Tweet map(String tweetJsonStr) throws Exception {
+            JsonNode tweetJson = mapper.readTree(tweetJsonStr);
+            JsonNode textNode = tweetJson.get("text");
+            JsonNode langNode = tweetJson.get("lang");
+
+            String text = textNode == null ? "" : textNode.getTextValue();
+            String lang = langNode == null ? "" : langNode.getTextValue();
+
+            return new Tweet(lang, text);
+        }
     }
 }
 
