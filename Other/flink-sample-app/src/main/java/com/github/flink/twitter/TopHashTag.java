75933896ee4e99475ab66bb12e59b4afa3677345
Jim Shaw <jimxshaw@gmail.com>
2020-06-21 20:06:46 -0500

    Initial implementation of TopHashTag flatmap.

--- Other/flink-sample-app/src/main/java/com/github/flink/twitter/TopHashTag.java
+++ Other/flink-sample-app/src/main/java/com/github/flink/twitter/TopHashTag.java
@@ -1,8 +1,11 @@
 package com.github.flink.twitter;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.twitter.TwitterSource;
+import org.apache.flink.util.Collector;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileReader;
@@ -27,7 +30,25 @@ public class TopHashTag {
 
         env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime);
 
-
+        // We need to convert a stream of tweets into a stream of hashtags.
+        // We use a flatmap to allow us to convert every incoming item into
+        // zero, one or more result elements. The type parameter is input
+        // type and output type. The input type is hashtag name and an integer.
+        env.addSource(new TwitterSource(props))
+                .map(new MapToTweet())
+                .flatMap(new FlatMapFunction<Tweet, Tuple2<String, Integer>>() {
+                    @Override
+                    public void flatMap(Tweet tweet, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                        // To create a stream of tags, we need to iterate over a list of tags and it
+                        // would output a tuple for every hashtag using the collector object.
+                        for (String tag : tweet.getTags()) {
+                            // We use the integer, in this case 1, to sum them later using
+                            // convenient Flink functions to get a count of tags.
+                            collector.collect(new Tuple2<>(tag, 1));
+                        }
+                    }
+                })
+                .print();
 
         env.execute();
     }
