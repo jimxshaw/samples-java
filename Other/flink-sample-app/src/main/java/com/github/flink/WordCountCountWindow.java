e8aae11b18c887aca6116382e1d329d7f0c5f8e2
Jim Shaw <jimxshaw@gmail.com>
2020-06-28 20:33:25 -0500

    Add count window class boilerplate.

---
+++ Other/flink-sample-app/src/main/java/com/github/flink/WordCountCountWindow.java
@@ -0,0 +1,28 @@
+package com.github.flink;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingAlignedProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+public class WordCountCountWindow {
+    public static void main(String[] args) throws Exception {
+        final ParameterTool params = ParameterTool.fromArgs(args);
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.getConfig().setGlobalJobParameters(params);
+
+        DataStream<String> dataStream = StreamUtil.getDataStream(env, params);
+
+        if (dataStream == null) {
+            System.exit(1);
+
+            return;
+        }
+
+        env.execute("Tumbling and Sliding Window");
+    }
+}
