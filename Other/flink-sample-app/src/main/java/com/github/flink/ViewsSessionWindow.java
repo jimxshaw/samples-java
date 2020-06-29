14f795f85e0d6a1f29470a29122a1984991f3b7a
Jim Shaw <jimxshaw@gmail.com>
2020-06-28 20:54:33 -0500

    Add ViewsSessionWindow class boilerplate.

---
+++ Other/flink-sample-app/src/main/java/com/github/flink/ViewsSessionWindow.java
@@ -0,0 +1,27 @@
+package com.github.flink;
+
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class ViewsSessionWindow {
+    public static void main(String[] args) throws Exception {
+        final ParameterTool params = ParameterTool.fromArgs(args);
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.getConfig().setGlobalJobParameters(params);
+
+        DataStream<String> dataStream = StreamUtil.getDataStream(env, params);
+
+        if (dataStream == null) {
+            System.exit(1);
+
+            return;
+        }
+
+
+
+        env.execute("Average Views per User and per User, per Page");
+    }
+}
