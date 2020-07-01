82fc02611730ee98cacd7d83297434d2c59ef40d
Jim Shaw <jimxshaw@gmail.com>
2020-07-01 17:11:54 -0500

    Add CarSpeedsWithCheckpoints boilerplate.

---
+++ Other/flink-sample-app/src/main/java/com/github/flink/CarSpeedsWithCheckpoints.java
@@ -0,0 +1,29 @@
+package com.github.flink;
+
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class CarSpeedsWithCheckpoints {
+    public static void main(String[] args) throws Exception {
+        final ParameterTool params = ParameterTool.fromArgs(args);
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.getConfig().setGlobalJobParameters(params);
+
+        DataStream<String> dataStream = StreamUtil.getDataStream(env, params);
+
+        if (dataStream == null) {
+            System.exit(1);
+
+            return;
+        }
+
+        DataStream<String> averageViewStream = dataStream.map(new Speed())
+                                                        .keyBy(0)
+                                                        .flatMap(new AverageSpeed());
+
+        env.execute("Car Speeds");
+    }
+}
