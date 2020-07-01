ad8fa0b67df39e464bbb8ca4367560d65df9dff5
Jim Shaw <jimxshaw@gmail.com>
2020-07-01 17:20:26 -0500

    Add AverageSpeed class and methods.

--- Other/flink-sample-app/src/main/java/com/github/flink/CarSpeedsWithCheckpoints.java
+++ Other/flink-sample-app/src/main/java/com/github/flink/CarSpeedsWithCheckpoints.java
@@ -1,10 +1,17 @@
 package com.github.flink;
 
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 public class CarSpeedsWithCheckpoints {
     public static void main(String[] args) throws Exception {
@@ -41,6 +48,53 @@ public class CarSpeedsWithCheckpoints {
             }
 
             return null;
+        }
+    }
+
+    public static class AverageSpeed extends RichFlatMapFunction<Tuple2<Integer, Double>, String> {
+        private transient ValueState<Tuple2<Integer, Double>> countSum;
+
+        @Override
+        public void flatMap(Tuple2<Integer, Double> input, Collector<String> out) throws Exception {
+            // Access the state value.
+            Tuple2<Integer, Double> currentCountSum = countSum.value();
+
+            // Print out the message if the car exceeds the speed limit.
+            if (input.f1 >= 65) {
+                out.collect(String.format(
+                        "EXCEEDED! The average speed of the last %s car(s) was %s," + "your speed is %s",
+                        currentCountSum.f0,
+                        currentCountSum.f1 / currentCountSum.f0,
+                        input.f1));
+
+                // Each time we encounter a speeding car, reset the cumulative
+                // and count each time we encounter a speeding car.
+                countSum.clear();
+                currentCountSum = countSum.value();
+            }
+            else {
+                out.collect("Thank you for staying under the speed limit!");
+            }
+
+            // Update the count.
+            currentCountSum.f0 += 1;
+
+            // Add the current speed of the car.
+            currentCountSum.f1 += input.f1;
+        }
+
+        public void open(Configuration config) {
+            // This allows us to initialize a value state.
+            ValueStateDescriptor<Tuple2<Integer, Double>> descriptor = new ValueStateDescriptor<>(
+                    /* The state name. */
+                    "carAverageSpeed",
+                    /* Type information. */
+                    TypeInformation.of(new TypeHint<Tuple2<Integer, Double>>() {
+                    }),
+                    Tuple2.of(0, 0.0) /* Default value of the state if nothing was set. */
+            );
+
+            countSum = getRuntimeContext().getState(descriptor);
         }
     }
 }
